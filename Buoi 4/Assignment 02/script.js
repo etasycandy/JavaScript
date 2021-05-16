406d233de93b29017e3e0065efc96f8d60c8f4e7
Trần Đình Hoàng <super.royal147@gmail.com>
2021-05-17 00:00:05 +0700

    Update script.js

--- Buoi 4/Assignment 02/script.js
+++ Buoi 4/Assignment 02/script.js
@@ -32,78 +32,39 @@ var clearr = function(){
     window.location.reload();
 }
 
-function getM(){
-    var date = document.getElementById("date").value;
-
-    date.getMonth();
-}
-
 // Thực hiện tính toán
 var tinhToan = function(){
-
-    // Xoá hết dữ liệu cũ
-    clearr();
-
-    // Dữ liệu đầu vào
-    var date = new Date(document.getElementById("date").value);
-    var time = document.getElementById("time").value;
-    var money = document.getElementById("tien-vay").value;
-    var lai = document.getElementById("lai-suat").value;
-
-    // get lấy thông số ngày, tháng, năm.
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-    var sDate = [];
-
-    // Định dạng hiển thị ngày (dd)
-    if (day < 10) {
-        day = '0' + day;
-    }
-    // Định dạng hiển thị tháng (mm)
-    if (month < 10) {
-        month = '0' + month;
-    }
-    // Định dạng hiển thị năm (yyyy)
-    if (year < 10) {
-        year = '000' + year;
-    } else if (year < 100) {
-        year = '00' + year;
-    } else if (year < 1000) {
-        year = '0' + year;
-    } else {
-        year = year;
-    }
-
-    sDate.push(day, month, year);
-
-    // xuất date "dd/mm/yyyy"
-    var date = sDate.join('/');
-
-    var tienTraHangThang = Math.round(money / time);
-    var moneyConLai = money;
-
-    for (let i = 0; i < 1; i++) {
-        var item0 = {
-            date,
-            i,
-            moneyConLai,
-        
-        }
-        result.push(item0);
-    }
     
-    for (let i = 1; i <= time; i++){
-        // xử lý ngày/tháng/năm
-        month = parseInt(sDate[1]);
-        year = parseInt(sDate[2]);
-        if (month >= 12) {
-            month = 1;
-            year += 1;
-        } else {
-            month += 1;
-        }
+    // (Required) Đặt điều kiện yêu cầu nhập đầy đủ các thông tin
+    if (document.getElementById("tien-vay").value == "") {
+        alert("Error! \nVui lòng nhập số tiền muốn vay.")
+    } else if (document.getElementById("time").value == "") {
+        alert("Error! \nVui lòng nhập thời gian vay.")
+    } else if (document.getElementById("lai-suat").value == "") {
+        alert("Error! \nVui lòng nhập lãi suất.")
+    } else if (document.getElementById("date").value == "") {
+        alert("Error! \nVui lòng nhập ngày giải ngân.")
+    } else {
 
+        // Xoá hết dữ liệu cũ
+        clearr();
+
+        // Dữ liệu đầu vào
+        var date = new Date(document.getElementById("date").value);
+        var time = document.getElementById("time").value;
+        var money = document.getElementById("tien-vay").value;
+        var lai = document.getElementById("lai-suat").value;
+
+        // get lấy thông số ngày, tháng, năm.
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        var sDate = [];
+
+        // Định dạng hiển thị ngày (dd)
+        if (day < 10) {
+            day = '0' + day;
+        }
         // Định dạng hiển thị tháng (mm)
         if (month < 10) {
             month = '0' + month;
@@ -119,32 +80,77 @@ var tinhToan = function(){
             year = year;
         }
 
-        sDate[1] = month;
-        sDate[2] = year;
-        
-        // xuất date "dd/mm/yyyy"
-        date = sDate.join('/');
+        sDate.push(day, month, year);
 
-        var tienLai = Math.round(moneyConLai * (lai / 100) / 12);
-        moneyConLai -= tienTraHangThang; 
-        if (moneyConLai < 10) {
-            tienTraHangThang += moneyConLai;
-            moneyConLai -= moneyConLai;
+        // xuất date "dd/mm/yyyy"
+        var date = sDate.join('/');
+
+        var tienTraHangThang = Math.round(money / time);
+        var moneyConLai = money;
+
+        for (let i = 0; i < 1; i++) {
+            var item0 = {
+                date,
+                i,
+                moneyConLai,
+                
+            }
+            result.push(item0);
         }
-        var total = tienTraHangThang + tienLai;
-        var item = {
-            date,
-            i,
-            moneyConLai,
-            tienTraHangThang,
-            tienLai,
-            total
-        }
-        result.push(item);
-        Save();
-        window.location.reload();
-        Total();
-        Display();
+    
+        for (let i = 1; i <= time; i++){
+            // xử lý ngày/tháng/năm
+            month = parseInt(sDate[1]);
+            year = parseInt(sDate[2]);
+            if (month >= 12) {
+                month = 1;
+                year += 1;
+            } else {
+                month += 1;
+            }
+
+            // Định dạng hiển thị tháng (mm)
+            if (month < 10) {
+                month = '0' + month;
+            }
+            // Định dạng hiển thị năm (yyyy)
+            if (year < 10) {
+                year = '000' + year;
+            } else if (year < 100) {
+                year = '00' + year;
+            } else if (year < 1000) {
+                year = '0' + year;
+            } else {
+                year = year;
+            }
+
+            sDate[1] = month;
+            sDate[2] = year;
+        
+            // xuất date "dd/mm/yyyy"
+            date = sDate.join('/');
+
+            var tienLai = Math.round(moneyConLai * (lai / 100) / 12);
+            moneyConLai -= tienTraHangThang; 
+            if (moneyConLai < 10) {
+                tienTraHangThang += moneyConLai;
+                moneyConLai -= moneyConLai;
+            }
+            var total = tienTraHangThang + tienLai;
+            var item = {
+                date,
+                i,
+                moneyConLai,
+                tienTraHangThang,
+                tienLai,
+                total
+            }
+            result.push(item);
+            Save();
+            window.location.reload();
+            Total();
+            Display();
+        }    
     }
 }
 
